189edca798d505f6386323e4ea433042c9d90ba8
Kirtika Ruchandani <kirtika@chromium.org>
2017-10-03 22:58:18 -0700

    CHROMIUM: Bump up logging level for roaming decisions
    Chrome OS relies on its connection manager, shill, to bump up the
    logging level in wpa_supplicant during 'interesting' times via its
    WiFi::SetConnectionDebugging method. The method sets wpa_supplicant
    debug level to 'info' (quieter) or 'debug' (when verbose).
    Currently, it is not guaranteed that SetConnectionDebugging will be
    called on all roaming paths, therefore we end up with two scenarios in
    the logs: one where roaming happens out of the blue, like so (timestamp
    removed):
    
    shill: [INFO:manager.cc(497)] Service  updated; state: Connected failure Unknown
    shill: [INFO:wifi.cc(9)] Enabling high bitrates.
    // Connected here
    shill: [INFO:service.cc(404)] Service : state Connected -> Online
    shill: [INFO:manager.cc(497)] Service  updated; state: Online failure Unknown
    shill: [INFO:wifi_service.cc(78)] Representative endpoint updated for service...
    shill: [INFO:manager.cc(74)] Default physical service:  (connected)
    shill: [INFO:wifi.cc(9)] Scan on wlan0 from RequestScan
    // We have roamed to a new AP that we are trying to authenticate with,
    // and we don't know why...
    wpa_supplicant: wlan0: SME: Trying to authenticate with <> (SSID='' freq= MHz)
    wpa_supplicant: wlan0: Trying to associate with <> (SSID='' freq= MHz)
    
    The second scenario, and the preferable one, is where we get details of
    how roaming decisions were made. This happens when
    WiFi::SetConnectionDebugging(true) is on the execution path, which seems
    to be only when connecting to a new service:
    
    wpa_supplicant: BSS: last_scan_res_used=14/32
    wpa_supplicant: wlan0: New scan results available (own=1 ext=0)
    wpa_supplicant: bgscan simple: scan result notification
    wpa_supplicant: bgscan simple: freq_idx 0
    wpa_supplicant: bgscan_poll_signal_monitor: bgscan poll noisefloor: 9999
    wpa_supplicant: wlan0: Radio work 'scan'<> done in <> seconds
    wpa_supplicant: wlan0: Selecting BSS from priority group <>
    wpa_supplicant: wlan0: 0: <MAC> ssid='' wpa_ie_len=<> rsn_ie_len=<> caps=<> level=<> freq=<>  wps
    wpa_supplicant: wlan0:    selected based on RSN IE
    wpa_supplicant: wlan0:    selected BSS <> ssid=''
    wpa_supplicant: wlan0: Allow reassociation - selected BSS has better estimated throughput
    wpa_supplicant: wlan0: Considering connect request: reassociate: 0  selected: <>  bssid: <> ...
    wpa_supplicant: wlan0: Request association with <>
    wpa_supplicant: wlan0: Re-association to the same ESS
    ...
    wpa_supplicant: wlan0: SME: Trying to authenticate with <> (SSID='' freq=<> MHz)
    
    Until we have confidence that all roaming scenarios do go through shill
    and can be intercepted with WiFi::SetConnectionDebugging(true), the
    safer route is to bump up the logging level of the following directly
    in wpa_supplicant itself:
      - roaming decisions.
      - estimated throughput and channel quality calculations.
    
    Signed-off-by: Kirtika Ruchandani <kirtika@chromium.org>
    (am from http://lists.infradead.org/pipermail/hostap/2019-December/040837.html)
    Signed-off-by: Nicolas Norvez <norvez@chromium.org>
    This was originally https://crrev.com/c/1955837 which was apparently
    rejected upstream. Make the CL a "CHROMIUM" patch and fix conflicts.
    
    BUG=chromium:771501
    BUG=b:193926134
    TEST=run wifi_matfunc on ${BOARD}
    
    Change-Id: I3566c40ad561708357ba405d61f079f464a685b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/3078576
    Tested-by: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Commit-Queue: Nicolas Norvez <norvez@chromium.org>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -1869,13 +1869,13 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 	int cur_snr = 0;
 	int ret = 0;
 
-	wpa_dbg(wpa_s, MSG_DEBUG, "Considering within-ESS reassociation");
-	wpa_dbg(wpa_s, MSG_DEBUG, "Current BSS: " MACSTR
+	wpa_dbg(wpa_s, MSG_INFO, "Considering within-ESS reassociation");
+	wpa_dbg(wpa_s, MSG_INFO, "Current BSS: " MACSTR
 		" freq=%d level=%d snr=%d est_throughput=%u",
 		MAC2STR(current_bss->bssid),
 		current_bss->freq, current_bss->level,
 		current_bss->snr, current_bss->est_throughput);
-	wpa_dbg(wpa_s, MSG_DEBUG, "Selected BSS: " MACSTR
+	wpa_dbg(wpa_s, MSG_INFO, "Selected BSS: " MACSTR
 		" freq=%d level=%d snr=%d est_throughput=%u",
 		MAC2STR(selected->bssid), selected->freq, selected->level,
 		selected->snr, selected->est_throughput);
@@ -1883,7 +1883,7 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 	if (wpa_s->current_ssid->bssid_set &&
 	    os_memcmp(selected->bssid, wpa_s->current_ssid->bssid, ETH_ALEN) ==
 	    0) {
-		wpa_dbg(wpa_s, MSG_DEBUG, "Allow reassociation - selected BSS "
+		wpa_dbg(wpa_s, MSG_INFO, "Allow reassociation - selected BSS "
 			"has preferred BSSID");
 		return 1;
 	}
@@ -1917,14 +1917,16 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 		cur_est = wpas_get_est_throughput_from_bss_snr(wpa_s,
 							       current_bss,
 							       cur_snr);
-		wpa_dbg(wpa_s, MSG_DEBUG,
+		wpa_dbg(wpa_s, MSG_INFO,
 			"Using signal poll values for the current BSS: level=%d snr=%d est_throughput=%u",
 			cur_level, cur_snr, cur_est);
 	}
 
 	if (sel_est > cur_est + 5000) {
-		wpa_dbg(wpa_s, MSG_DEBUG,
-			"Allow reassociation - selected BSS has better estimated throughput");
+		wpa_dbg(wpa_s, MSG_INFO,
+			"Allow reassociation - selected BSS has better "
+			"estimated throughput (current: %u, selected: %u)",
+			cur_est, sel_est);
 		return 1;
 	}
 
@@ -1932,7 +1934,7 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 
 	if (cur_level < 0 && cur_level > selected->level + to_5ghz * 2 &&
 	    sel_est < cur_est * 1.2) {
-		wpa_dbg(wpa_s, MSG_DEBUG, "Skip roam - Current BSS has better "
+		wpa_dbg(wpa_s, MSG_INFO, "Skip roam - Current BSS has better "
 			"signal level");
 		return 0;
 	}
@@ -1984,7 +1986,7 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 		min_diff -= 2;
 	diff = selected->level - cur_level;
 	if (diff < min_diff) {
-		wpa_dbg(wpa_s, MSG_DEBUG,
+		wpa_dbg(wpa_s, MSG_INFO,
 			"Skip roam - too small difference in signal level (%d < %d)",
 			diff, min_diff);
 		ret = 0;
