281fccc68f2e10b5f193ab8e74d4fb7e2e1a7508
Matthew Wang <matthewmwang@chromium.org>
2021-03-29 18:41:19 -0700

    FROMLIST: BACKPORT: Use default IEs in wpa_supplicant_trigger_scan
    wpa_supplicant_trigger_scan previously wouldn't include any of the IEs
    generated by wpa_supplicant_extra_ies. Instruct it to do so in most
    cases. This is necessary because MBO STAs are required to include MBO
    capabilities in their probe requests.
    
    (am from http://patchwork.ozlabs.org/patch/1520005)
    
    Signed-off-by: Matthew Wang <matthewmwang@chromium.org>
    Signed-off-by: Nicolas Norvez <norvez@chromium.org>
    
    Conflicts:
    - wpa_supplicant/scan.h
    	declaration of |wpa_add_scan_freqs_list()| had been added
    	upstream.
    
    BUG=b:180664130
    BUG=b:193926134
    TEST=Look at pcap
    TEST=all hwsim tests
    
    Change-Id: I56af87aed3587226349a364de96dc15582eb578c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/3078963
    Tested-by: Nicolas Norvez <norvez@chromium.org>
    Commit-Queue: Nicolas Norvez <norvez@chromium.org>
    Reviewed-by: Arowa Suliman <arowa@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>

--- wpa_supplicant/scan.h
+++ wpa_supplicant/scan.h
@@ -45,7 +45,8 @@ void wpa_supplicant_notify_scanning(struct wpa_supplicant *wpa_s,
 				    int scanning);
 struct wpa_driver_scan_params;
 int wpa_supplicant_trigger_scan(struct wpa_supplicant *wpa_s,
-				struct wpa_driver_scan_params *params);
+				struct wpa_driver_scan_params *params,
+				bool default_ies);
 struct wpa_scan_results *
 wpa_supplicant_get_scan_results(struct wpa_supplicant *wpa_s,
 				struct scan_info *info, int new_scan);
@@ -92,5 +93,6 @@ int wpa_add_scan_freqs_list(struct wpa_supplicant *wpa_s,
 			    enum hostapd_hw_mode band,
 			    struct wpa_driver_scan_params *params,
 			    bool is_6ghz);
+struct wpabuf * wpa_supplicant_extra_ies(struct wpa_supplicant *wpa_s);
 
 #endif /* SCAN_H */
