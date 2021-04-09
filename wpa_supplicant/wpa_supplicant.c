f48b1cb7f306de6231f9eb7535d8e839d61f75c7
Joshua Emele <jemele@chromium.org>
2021-04-09 18:18:13 +0000

    CHROMIUM: Override SSID logging
    This is a short-term stop-gap intended to reduce PII in wpa_supplicant
    logs. Because wpa_ssid_txt is generally used for logging, co-opt this
    symbol to hash SSID information.
    
    For control channel data, provide and use explicit control symbols
    (i.e., wpa_ctrl_ssid_txt). To support testing, any call site that uses
    both wpa_msg* and wpa_ssid_txt is converted to wpa_msg*_ctrl to avoid
    logging.
    
    Note that wpa_dbg behaves like wpa_msg, but it can be removed from build
    to reduce binary size. As such, it is not a control interface and does
    not require update.
    
    wlan0: Selecting BSS from priority group 0
    wlan0: 0: 62:b7:6e:cd:c7:b0 ssid='03600e8c3c95' wpa_ie_len=0...
    wlan0:    skip - SSID mismatch
    wlan0: 1: 60:b7:6e:cd:c7:b1 ssid='31dc8895fb56' wpa_ie_len=0...
    wlan0:    selected based on RSN IE
    wlan0:    selected BSS 60:b7:6e:cd:c7:b1 ssid='31dc8895fb56'
    
    BUG=b:183219113
    TEST=`auth_serv/update.sh` succeeds.
    TEST=`module_wpa_supplicant module_hostapd scan_random_mac` succeeds.
    TEST=`-f oce scan owe wpas_wmm_ac bgscan kernel wep ieee8021x ieee8021x\
    	monitor_interface wpas_config pmksa_cache dfs sae ap_ft ssid\
    	cfg80211 radius eap_proto connect_cmd autoscan` succeeds.
    TEST=`-f dbus` succeeds (`failed tests: dbus_network`).
    
    Change-Id: Iac66608f0d091c54ad4a83936608b9d1150803f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/2818535
    Tested-by: Joshua Emele <jemele@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Brian Norris <briannorris@chromium.org>
    (cherry picked from commit d67cf16f0347f3f29324fdfc393e7e65e62bde76)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/2826705
    Reviewed-by: Joshua Emele <jemele@chromium.org>
    Commit-Queue: Joshua Emele <jemele@chromium.org>
    Auto-Submit: Joshua Emele <jemele@chromium.org>

--- wpa_supplicant/wpa_supplicant.c
+++ wpa_supplicant/wpa_supplicant.c
@@ -2047,9 +2047,12 @@ void wpa_supplicant_associate(struct wpa_supplicant *wpa_s,
 			return;
 		}
 		wpa_s->current_bss = bss;
-		wpa_msg(wpa_s, MSG_INFO, MESH_GROUP_STARTED "ssid=\"%s\" id=%d",
-			wpa_ssid_txt(ssid->ssid, ssid->ssid_len),
-			ssid->id);
+		wpa_printf(wpa_s, MSG_INFO, MESH_GROUP_STARTED "ssid=\"%s\" id=%d",
+			   wpa_ssid_txt(ssid->ssid, ssid->ssid_len),
+			   ssid->id);
+		wpa_msg_ctrl(wpa_s, MSG_INFO, MESH_GROUP_STARTED "ssid=\"%s\" id=%d",
+			     wpa_ctrl_ssid_txt(ssid->ssid, ssid->ssid_len),
+			     ssid->id);
 		wpas_notify_mesh_group_started(wpa_s, ssid);
 #else /* CONFIG_MESH */
 		wpa_msg(wpa_s, MSG_ERROR,
@@ -3081,9 +3084,12 @@ static void wpas_start_assoc_cb(struct wpa_radio_work *work, int deinit)
 #ifdef CONFIG_IEEE80211R
 		const u8 *ie, *md = NULL;
 #endif /* CONFIG_IEEE80211R */
-		wpa_msg(wpa_s, MSG_INFO, "Trying to associate with " MACSTR
-			" (SSID='%s' freq=%d MHz)", MAC2STR(bss->bssid),
-			wpa_ssid_txt(bss->ssid, bss->ssid_len), bss->freq);
+		wpa_printf(wpa_s, MSG_INFO, "Trying to associate with " MACSTR
+			   " (SSID='%s' freq=%d MHz)", MAC2STR(bss->bssid),
+			   wpa_ssid_txt(bss->ssid, bss->ssid_len), bss->freq);
+		wpa_msg_ctrl(wpa_s, MSG_INFO, "Trying to associate with " MACSTR
+			     " (SSID='%s' freq=%d MHz)", MAC2STR(bss->bssid),
+			     wpa_ctrl_ssid_txt(bss->ssid, bss->ssid_len), bss->freq);
 		bssid_changed = !is_zero_ether_addr(wpa_s->bssid);
 		os_memset(wpa_s->bssid, 0, ETH_ALEN);
 		os_memcpy(wpa_s->pending_bssid, bss->bssid, ETH_ALEN);
@@ -3112,8 +3118,10 @@ static void wpas_start_assoc_cb(struct wpa_radio_work *work, int deinit)
 		return;
 #endif /* CONFIG_WPS */
 	} else {
-		wpa_msg(wpa_s, MSG_INFO, "Trying to associate with SSID '%s'",
-			wpa_ssid_txt(ssid->ssid, ssid->ssid_len));
+		wpa_printf(wpa_s, MSG_INFO, "Trying to associate with SSID '%s'",
+			   wpa_ssid_txt(ssid->ssid, ssid->ssid_len));
+		wpa_msg_ctrl(wpa_s, MSG_INFO, "Trying to associate with SSID '%s'",
+			     wpa_ctrl_ssid_txt(ssid->ssid, ssid->ssid_len));
 		if (bss)
 			os_memcpy(wpa_s->pending_bssid, bss->bssid, ETH_ALEN);
 		else
@@ -7147,10 +7155,14 @@ void wpas_auth_failed(struct wpa_supplicant *wpa_s, char *reason)
 
 	ssid->disabled_until.sec = now.sec + dur;
 
-	wpa_msg(wpa_s, MSG_INFO, WPA_EVENT_TEMP_DISABLED
-		"id=%d ssid=\"%s\" auth_failures=%u duration=%d reason=%s",
-		ssid->id, wpa_ssid_txt(ssid->ssid, ssid->ssid_len),
-		ssid->auth_failures, dur, reason);
+	wpa_printf(wpa_s, MSG_INFO, WPA_EVENT_TEMP_DISABLED
+		   "id=%d ssid=\"%s\" auth_failures=%u duration=%d reason=%s",
+		   ssid->id, wpa_ssid_txt(ssid->ssid, ssid->ssid_len),
+		   ssid->auth_failures, dur, reason);
+	wpa_msg_ctrl(wpa_s, MSG_INFO, WPA_EVENT_TEMP_DISABLED
+		     "id=%d ssid=\"%s\" auth_failures=%u duration=%d reason=%s",
+		     ssid->id, wpa_ctrl_ssid_txt(ssid->ssid, ssid->ssid_len),
+		     ssid->auth_failures, dur, reason);
 }
 
 
@@ -7161,9 +7173,12 @@ void wpas_clear_temp_disabled(struct wpa_supplicant *wpa_s,
 		return;
 
 	if (ssid->disabled_until.sec) {
-		wpa_msg(wpa_s, MSG_INFO, WPA_EVENT_REENABLED
-			"id=%d ssid=\"%s\"",
-			ssid->id, wpa_ssid_txt(ssid->ssid, ssid->ssid_len));
+		wpa_printf(wpa_s, MSG_INFO, WPA_EVENT_REENABLED
+			   "id=%d ssid=\"%s\"",
+			   ssid->id, wpa_ssid_txt(ssid->ssid, ssid->ssid_len));
+		wpa_msg_ctrl(wpa_s, MSG_INFO, WPA_EVENT_REENABLED
+			     "id=%d ssid=\"%s\"",
+			     ssid->id, wpa_ctrl_ssid_txt(ssid->ssid, ssid->ssid_len));
 	}
 	ssid->disabled_until.sec = 0;
 	ssid->disabled_until.usec = 0;
