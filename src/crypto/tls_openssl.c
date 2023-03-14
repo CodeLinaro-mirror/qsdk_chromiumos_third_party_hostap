f02f2715ff3ec0af783be4da9e6961b11a553977
David Ruth <druth@chromium.org>
2023-03-14 20:36:42 +0000

    FROMLIST: Compile-time config for libraries.
    Prevent loading arbitrary executable code based on config at runtime,
    while allowing libraries to be specified at compile time when they are
    known in advance.
    
    * Add the ability to configure libraries to load at compile time.
    	* CONFIG_PKCS11_ENGINE_PATH - pkcs11_engine library location.
    	* CONFIG_PKCS11_MODULE_PATH - pkcs11_module library location.
    	* CONFIG_OPENSC_ENGINE_PATH - opensc_engine library location.
    * Add flags with the ability to set each of the libraries to NULL and
      prevent loading them at runtime.
      	* CONFIG_NO_PKCS11_ENGINE_PATH - prevents loading pkcs11_engine
    	  library.
    	* CONFIG_NO_PKCS11_MODULE_PATH - prevents loading pkcs11_module
    	  library.
    	* CONFIG_NO_OPENSC_ENGINE_PATH - prevents loading opensc_engine
    	  library.
    	* CONFIG_NO_LOAD_DYNAMIC_EAP - prevents loading eap libraries at
    	  runtime.
    Signed-off-by: David Ruth <druth@chromium.org>
    (am from https://patchwork.ozlabs.org/patch/1765240/)
    (also found at
    https://marc.info?i=20230404233535.3084185-1-druth@chromium.org)
    
    BUG=chromium:1398996
    TEST=emerge-$BOARD wpa_supplicant-cros
    TEST=cros deploy $DUT wpa_supplicant-cros
    TEST=tast run -var "router=$ROUTER" -var "pcap=$PCAP" $DUT wifi.SimpleConnect.*eap*
    
    Cq-Depend: chromium:4338721, chromium:4338412
    Change-Id: I7b03e7eadba2407bed19662ef5ddac578b2d8d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/4734139
    Tested-by: David Ruth <druth@chromium.org>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: David Ruth <druth@chromium.org>

--- src/crypto/tls_openssl.c
+++ src/crypto/tls_openssl.c
@@ -1134,12 +1134,26 @@ void * tls_init(const struct tls_config *conf)
 	wpa_printf(MSG_DEBUG, "ENGINE: Loading builtin engines");
 	ENGINE_load_builtin_engines();
 
-	if (conf &&
-	    (conf->opensc_engine_path || conf->pkcs11_engine_path ||
-	     conf->pkcs11_module_path)) {
-		if (tls_engine_load_dynamic_opensc(conf->opensc_engine_path) ||
-		    tls_engine_load_dynamic_pkcs11(conf->pkcs11_engine_path,
-						   conf->pkcs11_module_path)) {
+#ifdef CONFIG_OPENSC_ENGINE_PATH
+	char const * const opensc_engine_path = CONFIG_OPENSC_ENGINE_PATH;
+#else
+	char const * const opensc_engine_path = (conf) ? conf->opensc_engine_path: NULL;
+#endif /* CONFIG_OPENSC_ENGINE_PATH */
+#ifdef CONFIG_PKCS11_ENGINE_PATH
+	char const * const pkcs11_engine_path = CONFIG_PKCS11_ENGINE_PATH;
+#else
+	char const * const pkcs11_engine_path = (conf) ? conf->pkcs11_engine_path: NULL;
+#endif /* CONFIG_PKCS11_ENGINE_PATH */
+#ifdef CONFIG_PKCS11_MODULE_PATH
+	char const * const pkcs11_module_path = CONFIG_PKCS11_MODULE_PATH;
+#else
+	char const * const pkcs11_module_path = (conf) ? conf->pkcs11_module_path : NULL;
+#endif /* CONFIG_PKCS11_MODULE_PATH */
+
+	if (opensc_engine_path || pkcs11_engine_path || pkcs11_module_path) {
+		if (tls_engine_load_dynamic_opensc(opensc_engine_path) ||
+		    tls_engine_load_dynamic_pkcs11(pkcs11_engine_path,
+						   pkcs11_module_path)) {
 			tls_deinit(data);
 			return NULL;
 		}
