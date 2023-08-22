f0c2c9c5b34b4e4794a8aa865fd905e88d13f86d
Kaidong Wang <kaidong@chromium.org>
2023-08-22 15:31:11 +0000

    BACKPORT: FROMLIST: Adjust the SNR when comparing BSSes based on Tx power config
    The max transmit power of Standard Power (SP) Access Points (AP) in the
    6 GHz Band is limited by effective isotropic radiated power (EIRP),
    while the max transmit power of Low Power Indoor (LPI) APs in the 6 GHz
    Band is limited by power spectral density (PSD). Therefore the max
    transmit power of LPI APs grows as the channel width increases.
    
    Adjust the SNR of BSSes when comparing BSSes based on Tx power config.
    
    Conflicts:
        wpa_supplicant/scan.c
    
    Backport Note: fix the garbled characters in comment
    
    BUG=b:258987520
    TEST=emerge-brya wpa_supplicant-cros
    TEST=hwsim
    TEST=manual test with CL 4612066, the link to the logs is
    https://paste.googleplex.com/5353061890392064
    UPSTREAM-TASK=b:303516184
    
    (am from https://patchwork.ozlabs.org/patch/1838412/)
    Signed-off-by: Kaidong Wang <kaidong@chromium.org>
    Change-Id: Ia93c8c44e163864852ee6adb358fe57b473f1eb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/4591240
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>

--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -334,6 +334,7 @@ struct hostapd_hw_modes {
  * @flags: information flags about the BSS/IBSS (WPA_SCAN_*)
  * @bssid: BSSID
  * @freq: frequency of the channel in MHz (e.g., 2412 = channel 1)
+ * @max_cw: the max channel width supported by both the AP and the STA
  * @beacon_int: beacon interval in TUs (host byte order)
  * @caps: capability information field in host byte order
  * @qual: signal quality
@@ -370,6 +371,7 @@ struct wpa_scan_res {
 	unsigned int flags;
 	u8 bssid[ETH_ALEN];
 	int freq;
+	enum chan_width max_cw;
 	u16 beacon_int;
 	u16 caps;
 	int qual;
--- wpa_supplicant/scan.h
+++ wpa_supplicant/scan.h
@@ -30,6 +30,13 @@
  */
 #define GREAT_SNR 25
 
+/*
+ * IEEE 80211ax-2021 9.4.2.161 Transmit Power Envelope element indicates no max
+ * tx power limit if Maximum Transmit Power field is 63.5 dBm.
+ * The default tx power if it is not constrained by Transmit Power Envelope IE
+ */
+#define TX_POWER_NO_CONSTRAINT 64
+
 #define IS_2P4GHZ(n) (n >= 2412 && n <= 2484)
 #define IS_5GHZ(n) (n > 4000 && n < 5895)
 
@@ -88,7 +95,7 @@ void scan_est_throughput(struct wpa_supplicant *wpa_s,
 			 struct wpa_scan_res *res);
 unsigned int wpas_get_est_tpt(const struct wpa_supplicant *wpa_s,
 			      const u8 *ies, size_t ies_len, int rate,
-			      int snr, int freq);
+			      int snr, int freq, enum chan_width *max_cw);
 void wpa_supplicant_set_default_scan_ies(struct wpa_supplicant *wpa_s);
 int wpa_add_scan_freqs_list(struct wpa_supplicant *wpa_s,
 			    enum hostapd_hw_mode band,
@@ -96,5 +103,10 @@ int wpa_add_scan_freqs_list(struct wpa_supplicant *wpa_s,
 			    bool is_6ghz, bool only_6ghz_psc,
 			    bool exclude_radar);
 struct wpabuf * wpa_supplicant_extra_ies(struct wpa_supplicant *wpa_s);
+int wpas_channel_width_offset(enum chan_width cw);
+int wpas_channel_width_rssi_bump(const u8 *ies, size_t ies_len,
+				 enum chan_width cw);
+int wpas_adjust_snr_by_chanwidth(const u8 *ies, size_t ies_len,
+				 enum chan_width max_cw, int snr);
 
 #endif /* SCAN_H */
