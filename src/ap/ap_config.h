cde31b9c37d02bc357df3cee9581d5b17a733bc7
Ruth Mekonnen <rmekonnen@google.com>
2024-01-10 02:14:08 +0000

    BACKPORT: FROMLIST: hostapd: Add require_eht configuration
    Add the ability to require EHT, advertising that via the
    BSS membership selector as well as rejecting association
    without EHT.
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.corp-partner.google.com>
    Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    
    (am from https://www.spinics.net/lists/hostap/msg10505.html)
    Signed-off-by: Ruth Mekonnen <rmekonnen@google.com>
    
    Contextual Conflicts:
    1. ap_config.h require_eht is not immediately followed
       by #endif /* CONFIG_IEEE80211BE */ because punct_bitmap
       and punct_acs_threshold were added after the original patchset
    2. ieee802_11.c modified __check_assoc_ies instead of check_assoc_ies
    3. ieee802_11_defs.h put BSS_MEMBERSHIP_SELECTOR_EHT_PHY before
       BSS_MEMBERSHIP_SELECTOR_HE_PHY because ChromeOS defines these
       constants in reverse order from the original patchset
    
    BUG=b:305821231
    TEST=apply chromium:5186357 and chromium:5208531, emerge and deploy
    then tast run $DUT wifi.SimpleConnect.80211beeht*pure should
     - pass on an EHT chromebook
     - fail on a non-EHT chromebook due to hardware not supporting EHT Phy
    UPSTREAM-TASK=b:319493993
    
    Change-Id: Idf9705600835dcca986d00bfbe8e2d762e3e4074
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/5208530
    Tested-by: Ruth Mekonnen <rmekonnen@google.com>
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Commit-Queue: Ruth Mekonnen <rmekonnen@google.com>
    Reviewed-by: Jun Yu <junyuu@chromium.org>

--- src/ap/ap_config.h
+++ src/ap/ap_config.h
@@ -1171,6 +1171,7 @@ struct hostapd_config {
 	enum oper_chan_width eht_oper_chwidth;
 	u8 eht_oper_centr_freq_seg0_idx;
 	struct eht_phy_capabilities_info eht_phy_capab;
+	int require_eht;
 	u16 punct_bitmap; /* a bitmap of disabled 20 MHz channels */
 	u8 punct_acs_threshold;
 #endif /* CONFIG_IEEE80211BE */
--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -120,6 +120,10 @@ u8 * hostapd_eid_supp_rates(struct hostapd_data *hapd, u8 *eid)
 #ifdef CONFIG_IEEE80211AX
 	if (hapd->iconf->ieee80211ax && hapd->iconf->require_he)
 		num++;
+#endif
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->iconf->ieee80211be && hapd->iconf->require_eht)
+		num++;
 #endif /* CONFIG_IEEE80211AX */
 	h2e_required = (hapd->conf->sae_pwe == SAE_PWE_HASH_TO_ELEMENT ||
 			hostapd_sae_pw_id_in_use(hapd->conf) == 2) &&
@@ -160,6 +164,13 @@ u8 * hostapd_eid_supp_rates(struct hostapd_data *hapd, u8 *eid)
 	}
 #endif /* CONFIG_IEEE80211AX */
 
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->iconf->ieee80211be && hapd->iconf->require_eht && count < 8) {
+		count++;
+		*pos++ = 0x80 | BSS_MEMBERSHIP_SELECTOR_EHT_PHY;
+	}
+#endif
+
 	if (h2e_required && count < 8) {
 		count++;
 		*pos++ = 0x80 | BSS_MEMBERSHIP_SELECTOR_SAE_H2E_ONLY;
@@ -186,6 +197,10 @@ u8 * hostapd_eid_ext_supp_rates(struct hostapd_data *hapd, u8 *eid)
 		num++;
 #ifdef CONFIG_IEEE80211AX
 	if (hapd->iconf->ieee80211ax && hapd->iconf->require_he)
+		num++;
+#endif
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->iconf->ieee80211be && hapd->iconf->require_eht)
 		num++;
 #endif /* CONFIG_IEEE80211AX */
 	h2e_required = (hapd->conf->sae_pwe == SAE_PWE_HASH_TO_ELEMENT ||
@@ -230,6 +245,14 @@ u8 * hostapd_eid_ext_supp_rates(struct hostapd_data *hapd, u8 *eid)
 			*pos++ = 0x80 | BSS_MEMBERSHIP_SELECTOR_HE_PHY;
 	}
 #endif /* CONFIG_IEEE80211AX */
+
+#ifdef CONFIG_IEEE80211BE
+	if (hapd->iconf->ieee80211be && hapd->iconf->require_eht) {
+		count++;
+		if (count > 8)
+			*pos++ = 0x80 | BSS_MEMBERSHIP_SELECTOR_EHT_PHY;
+	}
+#endif
 
 	if (h2e_required) {
 		count++;
@@ -3920,6 +3943,13 @@ static int __check_assoc_ies(struct hostapd_data *hapd, struct sta_info *sta,
 					  elems->eht_capabilities_len);
 		if (resp != WLAN_STATUS_SUCCESS)
 			return resp;
+
+		if (hapd->iconf->require_eht && !(sta->flags & WLAN_STA_EHT)) {
+			hostapd_logger(hapd, sta->addr, HOSTAPD_MODULE_IEEE80211,
+				       HOSTAPD_LEVEL_INFO, "Station does not support "
+				       "mandatory EHT PHY - reject association");
+			return WLAN_STATUS_DENIED_EHT_NOT_SUPPORTED;
+		}
 
 		if (!link) {
 			resp = hostapd_process_ml_assoc_req(hapd, elems, sta);
