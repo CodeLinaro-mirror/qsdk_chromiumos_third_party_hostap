1ff1db0570c67d3ae528c226a63d0492d3f2905e
Ilan Peer <ilan.peer@intel.com>
2024-04-08 16:06:56 +0300

    CHROMIUM: wpa_supplicant: Do not roam to an associated link
    When considering to roam to a different BSS and the connection
    is an MLD connection, do not roam to a BSS which is already
    included in the MLD connection.
    
    BUG=b:333841237, b:342367725
    TEST=emerge-$BOARD wpa_supplicant-cros
    
    (upstream commit 6f3e7c5d3ec04e48506b7cd364ccb7262b3dbccf)
    
    Change-Id: I858e9fd284f7b23d4c72b8319f07d790ce39cbe5
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Signed-off-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/5600779
    Reviewed-by: Jintao Lin <jintaolin@chromium.org>
    Reviewed-by: Andrzej Ostruszka <andrzejo@google.com>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -2391,6 +2391,11 @@ int wpa_supplicant_need_to_roam_within_ess(struct wpa_supplicant *wpa_s,
 		MAC2STR(selected->bssid), selected->freq, selected->level,
 		selected->snr, selected->est_throughput);
 
+	if (wpas_ap_link_address(wpa_s, selected->bssid)) {
+		wpa_dbg(wpa_s, MSG_DEBUG, "MLD: associated to selected BSS");
+		return 0;
+	}
+
 	if (wpa_s->current_ssid->bssid_set &&
 	    os_memcmp(selected->bssid, wpa_s->current_ssid->bssid, ETH_ALEN) ==
 	    0) {
