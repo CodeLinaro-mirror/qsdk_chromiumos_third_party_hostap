46cafbd2d569002fc61f61f5678ede46e96ba0ad
Ilan Peer <ilan.peer@intel.com>
2024-04-08 16:07:01 +0300

    FROMLIST: wpa_supplicant: Do not allow fast associate before scanning 6 GHz
    In case the channel map was updated to include the 6 GHz but these channels
    were not scanned yet, do not allow fast associate.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    (am from https://patchwork.ozlabs.org/patch/1920842/)
    (also found at https://marc.info/?i=20240408130701.531194-7-andrei.otcheretianski@intel.com)
    
    BUG=b:333841237
    TEST=emerge-$BOARD wpa_supplicant-cros
    TEST=tast -verbose run localhost:9222 wifi.HostapHwsim.full
    USTREAM-TASK=b:334054514
    
    Change-Id: Id1d399e80a67e4ad7fa44c351e181985ef83d4d6
    Signed-off-by: Kaidong Wang <kaidong@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hostap/+/5448835
    Reviewed-by: Matthew Wang <matthewmwang@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>

--- wpa_supplicant/events.c
+++ wpa_supplicant/events.c
@@ -3073,6 +3073,9 @@ int wpa_supplicant_fast_associate(struct wpa_supplicant *wpa_s)
 			       wpa_s->conf->scan_res_valid_for_connect)) {
 		wpa_printf(MSG_DEBUG, "Fast associate: Old scan results");
 		return -1;
+	} else if (wpa_s->crossed_6ghz_dom) {
+		wpa_printf(MSG_DEBUG, "Fast associate: crossed 6 GHz dom");
+		return -1;
 	}
 
 	return wpas_select_network_from_last_scan(wpa_s, 0, 1, false, NULL);
@@ -5446,11 +5449,10 @@ void wpa_supplicant_update_channel_list(struct wpa_supplicant *wpa_s,
 			wpa_dbg(ifs, MSG_DEBUG,
 				"Channel list changed - restart sched_scan");
 			wpas_scan_restart_sched_scan(ifs);
-		} else if (ifs->scanning && !was_6ghz_enabled &&
-			   ifs->is_6ghz_enabled) {
-			/* Look for APs in the 6 GHz band */
+		} else if (!was_6ghz_enabled && ifs->is_6ghz_enabled) {
 			wpa_dbg(ifs, MSG_INFO,
-				"Channel list changed - trigger 6 GHz-only scan");
+				"Channel list changed: 6 GHz are enabled");
+
 			ifs->crossed_6ghz_dom = true;
 		}
 	}
