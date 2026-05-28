e4a68e9ae9e8d38f55d0d7162cfc9917f99bded4
Ilan Peer <ilan.peer@intel.com>
2026-05-28 08:54:28 +0300

    WPA: Allow RSNE with zero AKMs
    An RSN element with AKM count == 0 is a valid RSN element.
    Specifically, this is valid in case that the AP is configured
    with PQC AKMs where the AKM information is conveyed in the
    Security Profile element.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/common/wpa_common.c
+++ src/common/wpa_common.c
@@ -2473,7 +2473,7 @@ int wpa_parse_wpa_ie_rsn(const u8 *rsn_ie, size_t rsn_ie_len,
 		count = WPA_GET_LE16(pos);
 		pos += 2;
 		left -= 2;
-		if (count == 0 || count > left / RSN_SELECTOR_LEN) {
+		if (count > left / RSN_SELECTOR_LEN) {
 			wpa_printf(MSG_DEBUG, "%s: ie count botch (key mgmt), "
 				   "count %u left %u", __func__, count, left);
 			return -6;
