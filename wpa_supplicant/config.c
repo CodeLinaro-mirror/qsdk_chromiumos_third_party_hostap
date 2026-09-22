1g4-mirror/hostap

commit 7f6caedc1936a3dd22021a10beedc70a95ec4cb7

secprof: wpa_supplicant configuration of security profiles
Add a per-network configuration parameter security_profiles for an
explicit configuration of enabled security profiles. This can be used as
an alternative mechanism for older parameters like key_mgmt to enable a
specific list of security profiles. The older options can be used with
any AP while the new explicit security profile configuration is used
only with APs that support security profiles.

Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/config.c
@@ -1523,16 +1523,17 @@ static char * wpa_config_write_auth_alg(const struct parse_data *data,
 #endif /* NO_CONFIG_WRITE */
 
 
-static int * wpa_config_parse_int_array(const char *value)
+static int * wpa_config_parse_int_array_helper(const char *value,
+					       int terminator)
 {
-	int *freqs;
+	int *vals;
 	size_t used, len;
 	const char *pos;
 
 	used = 0;
 	len = 10;
-	freqs = os_calloc(len + 1, sizeof(int));
-	if (freqs == NULL)
+	vals = os_calloc(len + 1, sizeof(int));
+	if (!vals)
 		return NULL;
 
 	pos = value;
@@ -1542,25 +1543,32 @@ static int * wpa_config_parse_int_array(const char *value)
 		if (used == len) {
 			int *n;
 			size_t i;
-			n = os_realloc_array(freqs, len * 2 + 1, sizeof(int));
-			if (n == NULL) {
-				os_free(freqs);
+
+			n = os_realloc_array(vals, len * 2 + 1, sizeof(int));
+			if (!n) {
+				os_free(vals);
 				return NULL;
 			}
 			for (i = len; i <= len * 2; i++)
-				n[i] = 0;
-			freqs = n;
+				n[i] = terminator;
+			vals = n;
 			len *= 2;
 		}
 
-		freqs[used] = atoi(pos);
-		if (freqs[used] == 0)
+		vals[used] = atoi(pos);
+		if (vals[used] == terminator)
 			break;
 		used++;
 		pos = os_strchr(pos + 1, ' ');
 	}
 
-	return freqs;
+	return vals;
+}
+
+
+static int * wpa_config_parse_int_array(const char *value)
+{
+	return wpa_config_parse_int_array_helper(value, 0);
 }
 
 
@@ -1628,28 +1636,29 @@ static int wpa_config_parse_freq_list(const struct parse_data *data,
 
 
 #ifndef NO_CONFIG_WRITE
-static char * wpa_config_write_freqs(const struct parse_data *data,
-				     const int *freqs)
+
+static char * wpa_config_write_int_array(const struct parse_data *data,
+					 const int *vals, int terminator)
 {
 	char *buf, *pos, *end;
 	int i, ret;
 	size_t count;
 
-	if (freqs == NULL)
+	if (!vals)
 		return NULL;
 
 	count = 0;
-	for (i = 0; freqs[i]; i++)
+	for (i = 0; vals[i] != terminator; i++)
 		count++;
 
 	pos = buf = os_zalloc(10 * count + 1);
-	if (buf == NULL)
+	if (!buf)
 		return NULL;
 	end = buf + 10 * count + 1;
 
-	for (i = 0; freqs[i]; i++) {
-		ret = os_snprintf(pos, end - pos, "%s%u",
-				  i == 0 ? "" : " ", freqs[i]);
+	for (i = 0; vals[i] != terminator; i++) {
+		ret = os_snprintf(pos, end - pos, "%s%d",
+				  i == 0 ? "" : " ", vals[i]);
 		if (os_snprintf_error(end - pos, ret)) {
 			end[-1] = '\0';
 			return buf;
@@ -1658,6 +1667,13 @@ static char * wpa_config_write_freqs(const struct parse_data *data,
 	}
 
 	return buf;
+}
+
+
+static char * wpa_config_write_freqs(const struct parse_data *data,
+				     const int *freqs)
+{
+	return wpa_config_write_int_array(data, freqs, 0);
 }
 
 
@@ -1673,6 +1689,7 @@ static char * wpa_config_write_freq_list(const struct parse_data *data,
 {
 	return wpa_config_write_freqs(data, ssid->freq_list);
 }
+
 #endif /* NO_CONFIG_WRITE */
 
 
@@ -2593,6 +2610,35 @@ static char * wpa_config_write_pasn_groups(const struct parse_data *data,
 #endif /* CONFIG_PASN */
 
 
+static int wpa_config_parse_security_profiles(const struct parse_data *data,
+					      struct wpa_ssid *ssid, int line,
+					      const char *value)
+{
+	int *vals;
+
+	vals = wpa_config_parse_int_array_helper(value, -1);
+	if (!vals)
+		return -1;
+	if (vals[0] == -1) {
+		os_free(vals);
+		vals = NULL;
+	}
+	os_free(ssid->security_profiles);
+	ssid->security_profiles = vals;
+
+	return 0;
+}
+
+
+#ifndef NO_CONFIG_WRITE
+static char * wpa_config_write_security_profiles(const struct parse_data *data,
+						 struct wpa_ssid *ssid)
+{
+	return wpa_config_write_int_array(data, ssid->security_profiles, -1);
+}
+#endif /* NO_CONFIG_WRITE */
+
+
 /* Helper macros for network block parser */
 
 #ifdef OFFSET
@@ -2961,6 +3007,7 @@ static const struct parse_data ssid_fields[] = {
 #ifdef CONFIG_PASN
 	{ FUNC(pasn_groups) },
 #endif /* CONFIG_PASN */
+	{ FUNC(security_profiles) },
 };
 
 #undef OFFSET
@@ -3173,6 +3220,7 @@ void wpa_config_free_ssid(struct wpa_ssid *ssid)
 #ifdef CONFIG_PASN
 	os_free(ssid->pasn_groups);
 #endif /* CONFIG_PASN */
+	os_free(ssid->security_profiles);
 	bin_clear_free(ssid, sizeof(*ssid));
 }
 

wpa_supplicant/config_file.c
@@ -784,6 +784,17 @@ static void write_pasn_groups(FILE *f, struct wpa_ssid *ssid)
 #endif /* CONFIG_PASN */
 
 
+static void write_security_profiles(FILE *f, struct wpa_ssid *ssid)
+{
+	char *value = wpa_config_get(ssid, "security_profiles");
+
+	if (!value)
+		return;
+	fprintf(f, "\tsecurity_profiles=%s\n", value);
+	os_free(value);
+}
+
+
 static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid,
 				     struct wpa_config *config)
 {
@@ -1061,6 +1072,7 @@ static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid,
 #ifdef CONFIG_PASN
 	write_pasn_groups(f, ssid);
 #endif /* CONFIG_PASN */
+	write_security_profiles(f, ssid);
 #undef STR
 #undef INT
 #undef INT_DEF

wpa_supplicant/config_ssid.h
@@ -1406,6 +1406,13 @@ struct wpa_ssid {
 	 */
 	int *pasn_groups;
 #endif /* CONFIG_PASN */
+
+	/**
+	 * security_profiles - Array of enabled security profiles
+	 *
+	 * This is a -1 terminated int_array of enabled security profiles.
+	 */
+	int *security_profiles;
 };
 
 #endif /* CONFIG_SSID_H */

wpa_supplicant/wpa_supplicant.conf
@@ -1154,6 +1154,19 @@ fast_reauth=1
 # If not set, no constraint on the cipher, i.e., accept whichever cipher the AP
 # indicates.
 #
+#
+# security_profiles: list of enabled security profiles
+# Enabled security profiles can be configured explicitly in this space separated
+# list of security profile numbers or implicitly using other parameters (mainly,
+# key_mgmt). The explicit configuration is used only when the target AP supports
+# security profiles while the other parameters can be used regardless whether
+# the AP supports security profiles. For cases where only a security profile is
+# allowed to be used (e.g., PQC), only the explicit configuration can be used.
+# When both configuration approaches are used together, union of all enabled
+# options is allowed for a connection.
+# For example:
+#security_profiles=1 9
+#
 # psk: WPA preshared key; 256-bit pre-shared key
 # The key used in WPA-PSK mode can be entered either as 64 hex-digits, i.e.,
 # 32 bytes or as an ASCII passphrase (in which case, the real PSK will be

CodeLinaro-mirror/qsdk_chromiumos_third_party_hostap

commit 91855000dad32e20b8ca61f9149cd505bdef9309

wpa_supplicant: Fix termination of parsed integer arrays
The initial allocation was zero filled, so a list with a nonzero
terminator, e.g., -1 which is the case with security profiles, was
left with a 'terminator' and readers ran past the end.

Fix this by setting each entry in the allocated array to 'terminator'.

Fixes: 7f6caedc1936 ("secprof: wpa_supplicant configuration of security profiles")
Signed-off-by: Ilan Peer <ilan.peer@intel.com>

wpa_supplicant/config.c
@@ -1527,7 +1527,7 @@ static int * wpa_config_parse_int_array_helper(const char *value,
 					       int terminator)
 {
 	int *vals;
-	size_t used, len;
+	size_t used, len, i;
 	const char *pos;
 
 	used = 0;
@@ -1536,13 +1536,15 @@ static int * wpa_config_parse_int_array_helper(const char *value,
 	if (!vals)
 		return NULL;
 
+	for (i = 0; i <= len; i++)
+		vals[i] = terminator;
+
 	pos = value;
 	while (pos) {
 		while (*pos == ' ')
 			pos++;
 		if (used == len) {
 			int *n;
-			size_t i;
 
 			n = os_realloc_array(vals, len * 2 + 1, sizeof(int));
 			if (!n) {